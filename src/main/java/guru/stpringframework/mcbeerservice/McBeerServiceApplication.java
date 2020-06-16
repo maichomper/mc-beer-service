b3513c70c1f2cff1d48c6cfbf271f039a9146678
maichomper <maichomper@gmail.com>
2020-06-16 15:42:20 -0500

    Create with Spring Boot Initailzr

---
+++ src/main/java/guru/stpringframework/mcbeerservice/McBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package guru.stpringframework.mcbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McBeerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/stpringframework/mcbeerservice/McBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.stpringframework.mcbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
