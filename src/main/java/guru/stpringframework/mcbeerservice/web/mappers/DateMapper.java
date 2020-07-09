e7cfeb0a4c83f5d56e61a4e0d357260aed0f6434
maichomper <maichomper@gmail.com>
2020-07-09 13:32:17 -0500

    CI test

--- src/main/java/guru/stpringframework/mcbeerservice/web/mappers/DateMapper.java
+++ src/main/java/guru/stpringframework/mcbeerservice/web/mappers/DateMapper.java
@@ -6,6 +6,8 @@ import java.sql.Timestamp;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 
+
+
 @Component
 public class DateMapper {
     public OffsetDateTime asOffsetDateTime(Timestamp ts){
